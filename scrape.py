529e790270e7c110101efcfd358aa9931013928c
Paul Blackburn <paul89@vt.edu>
2019-01-26 17:41:47 -0500

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,7 +1,8 @@
-root@sendgrid-tutorial:~/code# cat scrape.py
 import sys, json, requests, time
 
 url = "https://api.ethermine.org/miner/:4A6944369Cac57436203c9651201b5C1FA63A754/dashboard/"
+offline = True
+threshold = 25000000
 
 while True:
     try:
@@ -13,7 +14,11 @@ while True:
     rate = data["data"]["statistics"][0]["currentHashrate"]
     print("Current Hashrate: " + str(rate))
 
-    if rate < 25000000:
+    if rate < threshold and not offline:
         exit(0)
+
+    if rate > threshold:
+        offline = False
+        print("Miner is online")
 
     time.sleep(20)
